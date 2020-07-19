32fc7bc35db6f39423becb7806c06cda2c62b43a
mike <myke_roly@hotmail.com>
2020-07-19 05:09:29 -0300

    config styled components

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,19 @@
 import { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  colors: {
+    dark: '#000',
+    light: '#ccc'
+  }
+}
 
 export function reportWebVitals(metric: any) {
   console.log(metric);
 }
 
 export default ({ Component , pageProps}: AppProps) => {
-  return <Component {...pageProps} />
+  return <ThemeProvider theme={theme}>
+    <Component {...pageProps} />
+  </ThemeProvider>
 }
