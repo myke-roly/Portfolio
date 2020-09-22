9fafe8be0a38bd2b27a572e5f4da022446e72988
mike <myke_roly@hotmail.com>
2020-09-21 23:29:33 -0300

    added data to projects

---
+++ __data/index.ts
@@ -0,0 +1,34 @@
+export default [
+  {
+    id: 0,
+    title: "Healthy App",
+    description: "Proyecto creado con un grupo de personas de FrontendCafe a modo aprendizaje, donde pude conectar con personas que tienen la misma pasion por la programacion y la colaboracion en equipo. Mi primera experiencia trabajando en equipo, donde encontre una mejor comunicacion con diferentes integrantes del proyecto, tanto con la parte de diseno, front y backen devs.",
+    live: "https://healthy-front.vercel.app/",
+    code: "https://github.com/Healthy-Dev",
+    tools: "JS"
+  },
+  {
+    id: 1,
+    title: "Healthy App",
+    description: "Proyecto creado con un grupo de personas de FrontendCafe a modo aprendizaje, donde pude conectar con personas que tienen la misma pasion por la programacion y la colaboracion en equipo. Mi primera experiencia trabajando en equipo, donde encontre una mejor comunicacion con diferentes integrantes del proyecto, tanto con la parte de diseno, front y backen devs.",
+    live: "https://healthy-front.vercel.app/",
+    code: "https://github.com/Healthy-Dev",
+    tools: "JS"
+  },
+  {
+    id: 2,
+    title: "Healthy App",
+    description: "Proyecto creado con un grupo de personas de FrontendCafe a modo aprendizaje, donde pude conectar con personas que tienen la misma pasion por la programacion y la colaboracion en equipo. Mi primera experiencia trabajando en equipo, donde encontre una mejor comunicacion con diferentes integrantes del proyecto, tanto con la parte de diseno, front y backen devs.",
+    live: "https://healthy-front.vercel.app/",
+    code: "https://github.com/Healthy-Dev",
+    tools: "JS"
+  },
+  {
+    id: 3,
+    title: "Healthy App",
+    description: "Proyecto creado con un grupo de personas de FrontendCafe a modo aprendizaje, donde pude conectar con personas que tienen la misma pasion por la programacion y la colaboracion en equipo. Mi primera experiencia trabajando en equipo, donde encontre una mejor comunicacion con diferentes integrantes del proyecto, tanto con la parte de diseno, front y backen devs.",
+    live: "https://healthy-front.vercel.app/",
+    code: "https://github.com/Healthy-Dev",
+    tools: "JS"
+  }
+]
