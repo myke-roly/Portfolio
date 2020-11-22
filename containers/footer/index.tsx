925ae19bd9131b5736b01a67d77ffab145c69006
mike <myke_roly@hotmail.com>
2020-11-22 11:25:24 -0300

    add year dinamic

--- containers/footer/index.tsx
+++ containers/footer/index.tsx
@@ -3,10 +3,12 @@ import { FooterStyled } from './styled';
 import LogoIcon from 'assets/icons/LogoIcon';
 
 const Footer: React.SFC = () => {
+  const year: Date = new Date();
+
   return (
     <FooterStyled>
       <div className="container">
-        <p> &copy; All rights reserved By mykeDev - 2020</p>
+        <p> &copy; All rights reserved By MikeDev - {year.getFullYear()}</p>
         <LogoIcon />
       </div>
     </FooterStyled>
