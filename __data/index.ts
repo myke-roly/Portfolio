0c010e1e8c2d2967cec10b5104ba6930a5d55fb3
myke-roly <myke_roly@hotmail.com>
2020-12-08 15:02:10 -0300

    detailt projects

--- __data/index.ts
+++ __data/index.ts
@@ -16,7 +16,7 @@ export const projects: Array<DataI> = [
     title: 'Healthy App',
     img: 'healthy-desktop',
     description:
-      'Proyecto creado con un grupo de personas de FrontendCafe a modo aprendizaje, donde pude conectar con personas que tienen la misma pasion por la programacion y la colaboracion en equipo. Mi primera experiencia trabajando en equipo, donde encontre una mejor comunicacion con diferentes integrantes del proyecto, tanto con la parte de diseno, front y backen devs.',
+      'With a group of people we met at "FRONTEND CAFE", we came up with the idea of ​​creating an application that would allow us to save cards with health tips for developers, in which I could learn how teamwork communicates with the different members of the project Both with the design team, both with the front and the back developers, it was an incredible experience in which I learned a lot and had a slight idea of ​​what it takes to work as a team and bring a product to production.',
     live: 'https://healthy.vercel.app/',
     code: 'https://github.com/Healthy-Dev',
     tools: ['react', 'redux-sagas', 'scss'],
@@ -26,7 +26,7 @@ export const projects: Array<DataI> = [
     title: 'Portfolio',
     img: 'portfolio',
     description:
-      'Desarrolle mi portfolio personal usando Next.js integrando Typescript como lenguaje, y para los estilos decidi usar styled-components, que me parece una herramienta muy poderosa a la hora de desarollar aplicaciones en React.',
+      'I developed my personal website to display my personal, contact information, and individual and group projects that I developed throughout my learning process. It is built with NextJs, Styled-components, and typescript.',
     live: 'https://myke-dev.vercel.app/',
     code: 'https://github.com/myke-roly/Portfolio',
     tools: ['next-js', 'styled-components', 'typescript'],
@@ -36,7 +36,7 @@ export const projects: Array<DataI> = [
     title: 'Movie App',
     img: 'movies-app',
     description:
-      'Con personas de Front-End Cafe, decidimos a modo de aprendizaje crear una aplicacion e integrar todos nuestros conocimientos de react y su ecosistema. A partir de ello tambien hicimos coworking juntandonos en algun lugar de Buenos Aires, fue una experiencia bastante contructiva compartiendo ideas y ayudandonos unos a otros.',
+      'With the folks at Front-End Cafe, we decided on a learning mode to create a web application and integrate all our knowledge of the ReactJS and its ecosystem. From this we also did coworking somewhere in Buenos Aires, it was a very constructive experience to share ideas and help each other.',
     live: 'https://movies-mike.herokuapp.com/',
     code: 'https://github.com/myke-roly/movie-group-react',
     tools: ['react', 'styled-components', 'scss'],
@@ -45,15 +45,7 @@ export const projects: Array<DataI> = [
     id: 4,
     title: 'Full Stack E-commerce',
     img: 'e-commerce',
-    description: `Con la necesidad de un familiar por tener un e-commerce, decidi aplicar todo mi conociemiento para crear una web que tenga todo lo necesario para que se le haga el trabajo mas sencillo, realizando diferentes tareas: 
-    1. Listar catalogo de productos 
-    2. Filtrar productos por categorias
-    3. Buscador de productos
-    4. Carrito de compras
-    5. Login/Regiter
-    6. Opciones de envio y pago para cada compra
-    7. Medios de Pago, atravez de Mercado Pago o por Whatsapp
-    `,
+    description: `I was presented with the opportunity to create a very complete product store, where the entire web can be implemented from scratch, starting with the design and all the implementation of the different functionalities that were required, such as seeing a list of products, and it has different functionalities such as filtering by various options, searching for a specific product. It also has an authentication system, and for the payment system I integrate all the functionality using the payment market api in which I can know how the online payment management works`,
     live: 'https://caps-arg.herokuapp.com/',
     code: 'https://github.com/myke-roly/capsEcommerce',
     tools: ['next-js', 'styled-components', 'node-js', 'mongo-db'],
@@ -63,7 +55,7 @@ export const projects: Array<DataI> = [
     title: 'Admin E-commerce',
     img: 'admin',
     description:
-      'Para complementar a this web, Se creo un administrador donde tienes control detallado sobre la web, se puede ver estadisticas de comprar o listar de productos, agregar, eliminar, editar ver stock y mucho mas. Esto para hacer el trabajo mas facil para el cliente',
+      "To complement this website https://movies-mike.herokuapp.com/, an administrator was created where you have detailed control over the website, you can view purchase statistics or product listing, add, delete, edit, view stock and much more. This to facilitate the client/'s work.",
     live: 'https://admin-caps.vercel.app/',
     code: 'https://github.com/myke-roly/dashboard-products',
     tools: ['react', 'firebase', 'scss', 'redux'],
@@ -73,7 +65,7 @@ export const projects: Array<DataI> = [
     title: 'Administrador de Gastos',
     img: 'expences',
     description:
-      'Me hacia falta un "Exel" mas detallado y facil uso para administrar mis gastos personales y de cualquiera que lo use. Con la ayuda de firebase se creo una base de datos simple, donde se puede almacenar los gatos de cada usuario, y poder tener acceso a su informacion y ver todas las estadisticas personales, solo es necesarion authenticarse para poder usar la app.',
+      'I needed a more detailed and user-friendly way to manage my personal expenses and those of anyone who uses it. With the help of firebase, a simple database was created, where the cats of each user can be stored, and being able to access their information and see all the personal statistics, it is only necessary to authenticate to be able to use the application.',
     live: 'https://expenttrent-app.vercel.app/',
     code: 'https://github.com/myke-roly/expenttrent-app',
     tools: ['typescript', 'scss', 'firebase', 'chart-js'],
@@ -83,7 +75,7 @@ export const projects: Array<DataI> = [
     title: 'Todo Vue',
     img: 'todo-vue',
     description:
-      'Primera proyecto en vue, a modo de aprendizaje hice un simple todo integrando los conceptos basicos de vue.',
+      'Create this application after taking a course and with the help of official documentation. implement the basic concepts of this technology and integrate tailwind to give styles more quickly',
     live: 'https://todo-vue-rose.vercel.app/#/',
     code: 'https://github.com/myke-roly/todo-vue',
     tools: ['vue', 'tailwind'],
